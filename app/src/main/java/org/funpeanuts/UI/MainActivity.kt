962f0703f5357c1ae080f9f321f1b80c9a765314
mdb1217 <70698151+mdb1217@users.noreply.github.com>
2021-04-03 02:21:44 +0900

    Initial Commit

---
+++ app/src/main/java/org/funpeanuts/UI/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.funpeanuts.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.funpeanuts.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
